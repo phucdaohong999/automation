101ca724c733612e6fb735b7789c107c1b4cd083
Dao Hong Phuc <48122205+phucdaohong999@users.noreply.github.com>
2025-01-03 17:47:08 +0700

    push code

---
+++ Automation.Core/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+using Newtonsoft.Json;
+
+namespace Automation.Core.Helpers
+{
+    public static class ConfigurationHelper
+    {
+        public static T GetValue<T>(string key)
+        {
+            var value = ConfigurationManager.AppSettings[key];
+            if (value is null)
+            {
+                return default(T);
+            }
+
+            return (T)Convert.ChangeType(value, typeof(T));
+        }
+
+    }
+}
+
---
+++ Automation.Core/Helpers/JsonHelper.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace Automation.Core.Helpers
+{
+    public class JsonHelper
+	{
+
+        public static T GetJsonValue<T>(string key)
+        {
+            string filePath = ConfigurationHelper.GetValue<string>("testdatajsonfilepath");
+            string fileContent = File.ReadAllText(filePath);
+
+            var jsonData = JsonConvert.DeserializeObject<Dictionary<string, string>>(fileContent);
+
+            // Get value by key
+            if (jsonData.TryGetValue(key, out string value))
+            {
+                return (T)Convert.ChangeType(value, typeof(T));
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+
+    }
+}
+
